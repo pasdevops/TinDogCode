9428e97823b05ccb3b55bc3a7082bdc68e19cb8a
Pas <askpeevee@gmail.com>
2022-10-29 08:16:36 +0100

    bootstrap code

---
+++ coding_note.js
@@ -0,0 +1,38 @@
+/* HTML SECTION*/
+
+1. Create a FOLDER - WEB_DEV (Parent Folder)
+
+2. Create a file inside WEB_DEV - index.html
+
+3. Inside index.html - add BOILER PLATE 
+
+/* CSS SECTION*/
+
+1. Create a FOLDER inside WEB_DEV folder e.g CSS 
+
+2. Inside CSS folder, create a file e.g. styles.css 
+
+3. Link HTML and CSS up together e.g link below:
+
+<link rel="stylesheet" href="styles.css"></link>
+
+NOTE: HTML and CSS folder needs to be in the same hiracycal strucure of the folder
+Then use link to link them up on the css file Path2D. 
+
+
+
+
+
+/* API SECTION*/
+
+
+
+/* JAVASCRIPT SECTIOM*/
+
+alert("Hello");
+
+alert = function 
+
+"Hello" = Message
+
+Semi-colon ; = End 
