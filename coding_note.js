63a5e5473d9f443f2367e81aa3d69e8e984a7426
Pas <askpeevee@gmail.com>
2022-10-29 16:44:52 +0100

    Coding note

--- coding_note.js
+++ coding_note.js
@@ -14,16 +14,108 @@
 
 3. Link HTML and CSS up together e.g link below:
 
+Internal CSS 
+    
+Where in an HTML document isthe correct place to refer to an internal style sheet ?
+    <head>
+        SECTION
+    </head> 
+    
 <link rel="stylesheet" href="styles.css"></link>
 
+4. The class attribute is mostly used to point to a class in a style sheet. 
+
+
 NOTE: HTML and CSS folder needs to be in the same hiracycal strucure of the folder
-Then use link to link them up on the css file Path2D. 
+Then use link to link them up on the css file Path.
+    
+
+CSS = CLASS SELECTORS
+
+The difference between Class and ID selector.
+
+An ID is only used to identify one single element in our HTML. 
+IDs are only used when one element on the page should have a particular style applied to it.
+
+A class can be used to identify more than one HTML element.
+        
+        
+
+e.g. hmtl
+    < img class="bacon">
+
+e.g. css
+        
+.bacon{
+    background 
+} 
+    
+e.g. hmtl
+    < img class="brocoli" >
+
+e.g. css
+.brocoli{
+    background 
+} 
 
 
+CSS = ID'S SELECTORS. 
+
+e.g. hmtl
+    < img class="brocoli" >
+
+e.g. css
+#brocoli{
+    background 
+} 
+
+A pseudo-class is used to define a special state of an element.
+
+For example, it can be used to:
+
+Style an element when a user mouses over it
+Style visited and unvisited links differently
+Style an element when it gets focus
+
+The syntax of pseudo - classes:
+
+a:hover {
+  color: #FF00FF;
+        
 
 
 
 /* API SECTION*/
+
+What Are the Different Types of APIs ?
+    
+1. Endpoint - Server
+2. Paths
+3. Parameters
+Parameters goes at the end of URL AFTER ? 
+Key value pair follows AFTER ?
+key value = pair
+https://example.org/some/page?key1=foo&key2
+    
+If you want to have more than 1 parameters, you separate the key values pair with $
+https://example.org/some/page?key1=foo&key2=bar&key3=baz
+    
+
+4. Authentication
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
